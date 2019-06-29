a8b42704638ba17be22e596a3a28a1d4ed809f92
Oho Suguru <oho.sugu@gmail.com>
2019-06-29 16:44:00 +0900

    MRTK HTMLAgilityKit NLua

---
+++ ProjectOrthoverse/Assets/Scripts/Test/LibImplTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using HtmlAgilityPack;
+
+public class LibImplTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var doc = new HtmlAgilityPack.HtmlDocument();
+        var context = new NLua.Lua();
+
+        this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
